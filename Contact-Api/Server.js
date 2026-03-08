087a6148a2411bea1fb93bc3fa60d4a4ee1ad69f
Faizan Ali <154216258+MFaizanali2@users.noreply.github.com>
2026-03-08 23:47:19 +0500

    Contact Api DB

--- Contact-Api/Server.js
+++ Contact-Api/Server.js
@@ -1,25 +1,40 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
+import bodyParser from "express";
+import { User } from "./Models/User.js";
 
 const app = express();
 
-app.use(express.urlencoded({extended:true}))
-dotenv.config()
+app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+dotenv.config();
 
 mongoose
-  .connect(
-    process.env.MONGODB,
-    {
-      dbName: "Nodejs_Course",
-    }
-  )
+  .connect(process.env.MONGODB, {
+    dbName: "Nodejs_Course",
+  })
   .then(() => console.log("Mongodb Connected"))
   .catch((error) => console.log(error));
 
-  app.get('/',(req,res)=>{
-    res.send('Star Create Contact API')
-  })
+app.get("/", (req, res) => {
+  res.json({ message: "Start Create Contact API" });
+});
+
+app.post("/api/user/register", async (req, res) => {
+  const { name, email, password } = req.body;
+
+  if (name === "" || email === "" || password === "")
+    return res.json({ message: "Please fill are input" });
+
+  let user = await User.findOne({ email });
+  if (user) return res.json({ message: "Email Already Exist", success: false });
+
+  user = await User.create({ name, email, password });
+
+  // console.log("Printing data", req.body);
+  res.json({ message: "User Created Succesfully...!", success: true, user });
+});
 
 const Port = process.env.PORT;
 
